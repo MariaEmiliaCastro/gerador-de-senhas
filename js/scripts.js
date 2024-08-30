8c33816771ddbdcdff3daffed63ee77deed609cd
Maria Emilia Castro <maria.castro@pipefy.com>
2024-08-30 19:00:05 -0300

    style: add style to generator layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const sliderEl = document.querySelector("#password-length");
+const sliderValue = document.querySelector(".password-generator__length-text");
+
+sliderEl.addEventListener("input", (event) => {
+  const tempSliderValue = event.target.value;
+  sliderValue.value = tempSliderValue;
+
+  const progress = (tempSliderValue / sliderEl.max) * 100;
+  sliderEl.style.background = `linear-gradient(to right, #fff ${progress}%, #2c1746 ${progress}%)`;
+});
+
+sliderValue.addEventListener("input", (event) => {
+  const tempSliderValue = event.target.value;
+  sliderEl.value = tempSliderValue;
+
+  const progress = (tempSliderValue / sliderEl.max) * 100;
+
+  sliderEl.style.background = `linear-gradient(to right, #fff ${progress}%, #2c1746 ${progress}%)`;
+});
